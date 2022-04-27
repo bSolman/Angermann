737249879bfc53981974c3fcb5ec6929a97fab4b
bSolman <bjorn.solman@gmail.com>
2022-04-27 18:13:35 +0200

    minor fixes. Refactored SongRepo and SongService so that tests towards existing data is more readable.

--- src/main/java/com/angermannalaget/angermann/repository/SongRepo.java
+++ src/main/java/com/angermannalaget/angermann/repository/SongRepo.java
@@ -1,6 +1,7 @@
 package com.angermannalaget.angermann.repository;
 
 import com.angermannalaget.angermann.model.Song;
+import lombok.NonNull;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,10 +14,14 @@ public interface SongRepo extends MongoRepository<Song, String> {
 
     Song getSongByName(String name);
 
-    void deleteById(String id);
+    void deleteById(@NonNull String id);
 
     Optional<Song> getSongById(String id);
 
     List<Song> getSongsBySongFor(String songFor);
+
+    boolean existsByName(String name);
+
+    boolean existsById(@NonNull String id);
 
 }
